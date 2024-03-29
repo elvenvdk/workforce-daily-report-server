e321cca6a0d0672728b5563022d3a0e27c7d1381
elvenvdk <vanderkuech@icloud.com>
2024-03-29 13:11:15 -0400

    Modified email

--- src/aws/emailService.ts
+++ src/aws/emailService.ts
@@ -62,7 +62,7 @@ export const sendEmail = async (body: any, messageRecipent: string) => {
           <div>
           <h1>Bissetta & List</h1>
           <h2>Workforce Daily Report Authorization</h2>
-          <a href="${prodMailUrl}/user-confirmation" />
+          <a href="${devMailUrl}/user-confirmation" />
           ${body?.text}
           <br />
           <br />
