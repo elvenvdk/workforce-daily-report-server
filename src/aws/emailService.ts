af2f91412c9ae596acb610b6b98cb261878d248e
elvenvdk <vanderkuech@icloud.com>
2024-04-08 12:18:11 -0400

    Updated emailService

--- src/aws/emailService.ts
+++ src/aws/emailService.ts
@@ -9,37 +9,6 @@ const mailer = nodemailer.createTransport({
   SES: new AWS.SES(),
 });
 
-const namesArr = ["alvin", "fedner", "margaret", "lilly"];
-
-const params = {
-  Destination: {
-    ToAddresses: [
-      // "vanderkuech@icloud.com",
-      "annecyops@gmail.com",
-      // 'fedner@bissettalist.com'
-    ],
-  },
-  Message: {
-    Body: {
-      Html: {
-        Charset: "UTF-8",
-        Data: `
-        <div>
-        <h1>This is a test</h1>
-        <ul>
-        ${namesArr.map(name => `<li>${name}</li>`)}
-        </ul>
-        </div>`,
-      },
-    },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "Test email",
-    },
-  },
-  Source: "no-reply@notifications.workforce-daily-report.com",
-};
-
 const devMailUrl = "http://localhost:3000";
 const prodMailUrl = "http://workforce-daily-report.com";
 
